dfa7a8b77c61a03ec3a07eb3fbf03d6c96ae8587
arafatkazi19 <arafatkazi47@gmail.com>
2021-11-18 22:37:24 +0600

    popular books shown in index page

--- inc/sidebar.php
+++ inc/sidebar.php
@@ -1,3 +1,4 @@
+
 <div class="col-lg-3 mt-2">
     <div class="search-option">
         <form method="post" action="search.php">
@@ -12,5 +13,32 @@
 
         </form>
 
+    </div>
+    <hr>
+    <div>
+        <h4 class="text-primary">Popular Books</h4>
+        <?php
+        $sql = "select books.*, sum(booking_list.book_id) as total from books inner join booking_list
+                 on books.id = booking_list.book_id group by booking_list.book_id order by total desc limit 3";
+        $books = mysqli_query($db, $sql);
+        while ($row=mysqli_fetch_assoc($books)){
+
+            ?>
+            <div class="card">
+                <div class="card-body">
+                        <?php if (!empty($row['image'])) { ?>
+                            <a href="details.php?id=<?php echo $row['id'] ?>"><img src="admin/dist/img/books/<?php echo $row['image']?>" width="100px"></a>
+
+                        <?php    } else { ?>
+                            <a href="details.php?id=<?php echo $row['id'] ?>"><img src="admin/dist/img/books/default_book.jpg" width="80px"></a>
+                        <?php   }?>
+                    <h4><a href="details.php?id=<?php echo $row['id'] ?>"><?php echo $row['title']; ?></a></h4>
+                        <p style="margin: 0;padding: 0"><?php echo $row['sub_title']; ?></p>
+                </div>
+            </div>
+        <?php   }
+
+
+        ?>
     </div>
 </div>
